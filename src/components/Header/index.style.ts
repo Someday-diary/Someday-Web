aabade75df5ae5138b5625a158c72b85bae9a9a4
Junhong0209 <junh040209@naver.com>
2021-12-20 13:46:41 +0900

    :sparkles: Add: Admin Page Hader
    어드민 페이지 용 헤더

---
+++ src/components/Header/index.style.ts
@@ -0,0 +1,22 @@
+import { colorPalette } from "src/styles/colorPalette";
+
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 80px;
+  border-bottom: 1px solid ${colorPalette.lineColor};
+  text-align: center;
+  line-height: 80px;
+`;
+
+export const HeaderItem = styled.div`
+  width: 100px;
+  height: 40px;
+  line-height: 40px;
+  display: inline-block;
+  border-radius: 8px;
+  margin-left: 45px;
+  background-color: ${props => props.style ? colorPalette.green1 : colorPalette.white};
+  color: ${props => props.style ? colorPalette.green3 : colorPalette.black};
+`;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+import * as S from 'src/components/Header/index.style';
+
+const Header = ({...props}) => {
+  const { title1, title2, title3 } = props
+
+  return (
+    <S.HeaderContainer>
+      <S.HeaderItem style={title1}>
+        <Link href='/admin/notice'>공지사항</Link>
+      </S.HeaderItem>
+      <S.HeaderItem style={title2}>
+        <Link href='/admin/notice'>CS문의</Link>
+      </S.HeaderItem>
+      <S.HeaderItem style={title3}>
+        <Link href='/admin/notice'>서버로그</Link>
+      </S.HeaderItem>
+    </S.HeaderContainer>
+  );
+};
+
+export default Header;

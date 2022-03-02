d9fe1f588f304f297f92bb4227d581f5b1916648
Junhong0209 <junh040209@naver.com>
2022-03-02 17:35:35 +0900

    Add: 채용 공고 페이지 타이틀 추가

---
+++ src/components/Main/Recruitment/Title/index.tsx
@@ -0,0 +1,16 @@
+import * as S from 'src/components/Main/Recruitment/Title/index.style';
+
+const RecruitmentTitle = () => {
+  return (
+    <>
+      <S.MainTitle>오늘하루 팀 채용</S.MainTitle>
+      <S.SubTitle>
+        <div>오늘하루는 대구소프트웨어마이스터고등학교의 동아리입니다.</div>
+        <div>사용자의 하루하루를 위해 새로운 서비스를 디자인/개발합니다.</div>
+        <div>새로운 서비스의 기획부터 개발가지 모든 과정에 참여할 7기 신입생들의 많은 지원 바랍니다.</div>
+      </S.SubTitle>
+    </>
+  );
+};
+
+export default RecruitmentTitle;

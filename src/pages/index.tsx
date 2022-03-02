328bb54624dbbf80cef076b4e608e9e585475e96
Junhong0209 <junh040209@naver.com>
2022-03-02 09:52:14 +0900

    Add: 메인 페이지 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
+import MainForm from 'src/components/Main';
+
+import MainHeader from 'src/components/Main/Header';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      hi
-    </div>
+    <>
+      <MainHeader />
+      <MainForm />
+    </>
   )
 }
 

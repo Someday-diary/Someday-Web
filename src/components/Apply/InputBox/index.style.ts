63b43fda48bc58f3104bc925602c826280d9eb40
Junhong0209 <junh040209@naver.com>
2022-03-05 11:55:34 +0900

    Update: 지원 페이지 UI 개선 및 코드 수정

--- src/components/Apply/InputBox/index.style.ts
+++ src/components/Apply/InputBox/index.style.ts
@@ -2,22 +2,43 @@ import { colorPalette } from "src/styles/colorPalette";
 import styled from "styled-components";
 
 export const InputBox = styled.div`
+  margin: auto;
+  margin-top: 30px;
+
   > div {
+    width: 70%;
+    margin: auto;
     display: flex;
     color: #454b54;
-    margin: 8px 0;
     text-align: left;
     justify-content: left;
     align-items: center;
+    font-family: pretendard600;
   }
 
   > input {
-    width: 100%;
+    width: 70%;
     font-size: 16px;
+    margin-top: 8px;
     padding: 16px;
     border: 2px solid ${colorPalette.mainHeaderLine};
     border-radius: 8px;
     background-color: #f9fafb;
+
+    :hover {
+      border: 2px solid #85E0C2;
+    }
+
+    :focus {
+      border: 2px solid #33CC99;
+    }
+
+    ::placeholder {
+      text-align: right;
+      font-family: Pretendard600;
+      font-size: 16px;
+      color: #9caec9;
+    }
   }
 `;
 
@@ -28,8 +49,4 @@ export const Dot = styled.div`
   height: 6px;
   border-radius: 10px;
   background-color: #f25a5a;
-`;
-
-export const A = styled.a`
-  color: coral;
 `;
--- src/components/Apply/InputBox/index.tsx
+++ src/components/Apply/InputBox/index.tsx
@@ -1,6 +1,6 @@
 import * as S from "src/components/Apply/InputBox/index.style";
 
-const InputBox = (props: { title: string }) => {
+const InputBox = (props: any) => {
   return (
     <>
       <S.InputBox>
@@ -8,7 +8,22 @@ const InputBox = (props: { title: string }) => {
           {props.title}
           <S.Dot></S.Dot>
         </div>
-        <input type="text" />
+        <input 
+          type={props.type} 
+          onClick={() => {
+            if (!sessionStorage.getItem(props.title)) {
+              sessionStorage.setItem(props.title, '1');
+            } else {
+              const num = Number(sessionStorage.getItem(props.title)) + 1;
+              sessionStorage.setItem(props.title, String(num));
+            }
+            props.onClick();
+            props.setter(props.title);
+          }} 
+          onChange={props.onChange} 
+          value={props.value}
+          placeholder={props.placeholder}
+        />
       </S.InputBox>
     </>
   );

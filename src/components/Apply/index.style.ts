0fd374d5bf66d20d3f276933e8cbd807c7514bfb
Junhong0209 <junh040209@naver.com>
2022-03-05 11:57:27 +0900

    Update: 퍼블리싱 및 api 연동을 위한 코드 작성

--- src/components/Apply/index.style.ts
+++ src/components/Apply/index.style.ts
@@ -1,17 +1,47 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { Layout } from "../Main/index.style";
 
 export const ApplyLayout = styled(Layout)`
   align-items: center;
   justify-content: center;
+  height: 100%;
 `;
 
 export const Title = styled.div`
   margin: 3rem;
   font-size: 1.5rem;
-  font-weight: 600;
+  font-family: Pretendard700;
 `;
 
-export const Content = styled.div`
+export const Content = styled.form`
   padding: 0 20rem;
+`;
+
+export const Message = styled.div`
+  width: 70%;
+  margin: auto; 
+  color: #5A78A5;
+  font-family: Pretendard600;
+  font-size: 12px;
+  text-align: left;
+  margin-top: 8px;
+  line-height: 16px;
+`;
+
+export const SubmitButton = styled.button`
+  margin-top: 50px;
+  width: 250px;
+  height: 50px;
+  font-family: Pretendard600;
+  font-size: 16px;
+  border-radius: 8px;
+
+  ${props => props.disabled ? css`
+    disabled;
+    background-color: #D6F5EB;
+    color: #FFFFFF;
+  ` : css`
+    background-color: #33CC99;
+    color: #FFFFFF;
+  `}
 `;
--- src/components/Apply/index.tsx
+++ src/components/Apply/index.tsx
@@ -1,13 +1,162 @@
+/* eslint-disable */
 import * as S from "src/components/Apply/index.style";
-import InputBox from "./InputBox";
 
-const ApplyForm = () => {
+import InputBox from "src/components/Apply/InputBox/index";
+import ErrorMessage from "src/components/Apply/ErrorMessage/index";
+import useInput from "src/hooks/useInput";
+
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import CustomModal from "../Modal";
+
+const ApplyForm = (props: any) => {
+  const [number, onChangeNumber] = useInput<string>('');
+  const [name, onChangeName] = useInput<string>('');
+  const [phoneNumber, onChangePhoneNumber] = useInput<string>('');
+  const [email, onChangeEmail] = useInput<string>('');
+
+  const [numberText, setNumberText] = useState<string>('');
+  const [nameText, setNameText] = useState<string>('');
+  const [phoneNumberText, setPhoneNumberText] = useState<string>('');
+  const [emailText, setEmailText] = useState<string>('');
+  const [countNumber, setCountNumber] = useState<number>(0);
+  const [countName, setCountName] = useState<number>(0);
+  const [countPhoneNumber, setCountPhoneNumber] = useState<number>(0);
+  const [countEmail, setCountEmail] = useState<number>(0);
+
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (sessionStorage)
+      setCountNumber(Number(sessionStorage?.getItem(numberText)));
+      setCountName(Number(sessionStorage?.getItem(nameText)));
+      setCountPhoneNumber(Number(sessionStorage?.getItem(phoneNumberText)));
+      setCountEmail(Number(sessionStorage?.getItem(emailText)));
+  });
+
+  const onClickNumberEvent = () => {
+    if (sessionStorage)
+      setCountNumber(Number(sessionStorage?.getItem(numberText)));
+  }
+  const onClickNameEvent = () => {
+    if (sessionStorage)
+      setCountName(Number(sessionStorage?.getItem(nameText)));
+  }
+  const onClickPhoneNumberEvent = () => {
+    if (sessionStorage)
+      setCountPhoneNumber(Number(sessionStorage?.getItem(phoneNumberText)));
+  }
+  const onClickEmailEvent = () => {
+    if (sessionStorage)
+      setCountEmail(Number(sessionStorage?.getItem(emailText)));
+  }
+
   return (
     <S.ApplyLayout>
-      <S.Title>UX/UI Designer 지원서</S.Title>
+      <S.Title>{props.query} 지원서</S.Title>
       <S.Content>
-        <InputBox title="학번" />
+        <InputBox 
+          title="학번" 
+          onChange={onChangeNumber} 
+          value={number} 
+          onClick={onClickNumberEvent} 
+          setter={setNumberText} 
+          type="number" 
+        />
+        <S.Message>
+          <div>*학번은 학년, 반, 번호 순서대로 기입합니다.</div>
+          <div>*번호가 한자리수일 경우에 앞에 0을 기입후 번호를 기입합니다.</div>
+          <div>ex) 1학년 2반 5번 일 경우 &gt; 1205</div>
+        </S.Message>
+        {
+          countNumber === 0 || number.length !== 0 ? (
+            <></>
+          ) : (
+            <ErrorMessage text='*학번을 입력해주세요' />
+          )
+        }
+        <InputBox 
+          title="이름" 
+          onChange={onChangeName} 
+          value={name} 
+          onClick={onClickNameEvent} 
+          setter={setNameText} 
+          type="text" 
+        />
+        {
+          countName === 0 || name.length !== 0 ? (
+            <></>
+          ) : (
+            <ErrorMessage text='*이름을 입력해주세요' />
+          )
+        }
+        <InputBox 
+          title="전화번호" 
+          onChange={onChangePhoneNumber} 
+          value={phoneNumber} 
+          onClick={onClickPhoneNumberEvent} 
+          setter={setPhoneNumberText} 
+          type="number" 
+        />
+        {
+          countPhoneNumber === 0 || phoneNumber.length !== 0 ? (
+            <></>
+          ) : (
+            <ErrorMessage text='*전화번호을 입력해주세요' />
+          )
+        }
+        <InputBox 
+          title="이메일" 
+          onChange={onChangeEmail} 
+          value={email} 
+          onClick={onClickEmailEvent} 
+          setter={setEmailText} 
+          type="email" 
+          placeholder="@dgsw.hs.kr" 
+        />
+        {
+          countEmail === 0 || email.length !== 0 ? (
+            <ErrorMessage text='' />
+          ) : (
+            <ErrorMessage text='*이메일을 입력해주세요' />
+          )
+        }
+        {
+          number.length !== 0 && name.length !== 0 && phoneNumber.length !== 0 && email.length !== 0 ? (
+            <S.SubmitButton type='button' onClick={() => setModalIsOpen(true)}>제출 하기</S.SubmitButton>
+          ) : (
+            <S.SubmitButton disabled>제출 하기</S.SubmitButton>
+          )
+        }
+        {/* <S.SubmitButton disabled onClick={() => {
+          toast('동일한 학번으로 지원한 이력이 있습니다.',
+            {
+              style: {
+                borderRadius: '8px',
+                maxWidth: '516px',
+                height: '43px',
+                fontSize: '16px',
+                background: 'rgba(18, 24, 33, 0.8)',
+                color: '#FFFFFF'
+              }
+            }
+          );
+        }}>test</S.SubmitButton>
+        <Toaster position="bottom-center" toastOptions={{
+          style: {
+            width: '560px'
+          }
+        }} /> */}
       </S.Content>
+      <CustomModal 
+        modalIsOpen={modalIsOpen} 
+        setModalIsOpen={setModalIsOpen} 
+        title={props.query} 
+        number={number} 
+        name={name} 
+        phoneNumber={phoneNumber} 
+        email={email} 
+      />
     </S.ApplyLayout>
   );
 };

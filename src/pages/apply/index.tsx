700786b42b10419a5ae05810b190cd2a8a999cb4
Junhong0209 <junh040209@naver.com>
2022-03-06 15:33:36 +0900

    Delete: Router Query 제거

--- src/pages/apply/index.tsx
+++ src/pages/apply/index.tsx
@@ -2,15 +2,10 @@ import React from "react";
 import ApplyForm from "src/components/Apply";
 import MainHeader from "src/components/Main/Header";
 
-import { useRouter } from "next/router";
-
-const Apply = () => {
-  const router = useRouter();
-
-  return (
+const Apply = () => {return (
     <>
       <MainHeader />
-      <ApplyForm query={router.query.jobGroup} />
+      <ApplyForm />
     </>
   );
 };

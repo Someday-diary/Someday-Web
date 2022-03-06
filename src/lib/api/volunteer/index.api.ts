9d062d867c18ccd9558392ee20e049982d645541
Junhong0209 <junh040209@naver.com>
2022-03-06 15:34:34 +0900

    Update: API 추가 및 사용하지 않는 코드 삭제

--- src/lib/api/volunteer/index.api.ts
+++ src/lib/api/volunteer/index.api.ts
@@ -1,9 +1,16 @@
-import { request } from "src/lib/Axios";
+import { instance } from "src/lib/Axios";
 import { IPostVolunteer } from "src/types/volunteer";
 
 export const handlePostVolunteer = async (req: IPostVolunteer) => {
   const url: string = '/volunteer';
-  const { data } = await request.post(url, req);
+  const { data } = await instance.post(url, req);
+
+  return data;
+}
+
+export const handleDuplicateCheckVolunteer = async (req: string) => {
+  const url: string = `/volunteer?student_id=${req}`;
+  const { data } = await instance.get(url);
 
   return data;
 }
